235c347faf46721be14ed0082b33a028fdc01987
SeraphCelestial <trayden@drkmind.com>
2021-04-30 08:07:48 -0700

    Added manual spawn control to EnemySpawner gameobject

--- Assets/Trayden/Scripts/EnemyMovement.cs
+++ Assets/Trayden/Scripts/EnemyMovement.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public GameObject spawnItem;
-    public void Awake() 
-    {
-        spawnItem = GameObject.FindGameObjectWithTag("EnemySpawner");
-    }
-
     void Update()
     {
         if(gameObject.GetComponent<CharacterInfo>().health < 100)
         {
-            spawnItem.GetComponent<EnemySpawning>().StartCoroutine("SpawnEnemy");
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Trayden/Scripts/EnemySpawning.cs
+++ Assets/Trayden/Scripts/EnemySpawning.cs
@@ -7,6 +7,7 @@ public class EnemySpawning : MonoBehaviour
     public GameObject enemy;
     public float x;
     public float z;
+    public float spawnTime;
     void Start()
     {
         StartCoroutine("SpawnEnemy");
@@ -17,6 +18,7 @@ public class EnemySpawning : MonoBehaviour
         x = Random.Range(-34.5f, 35);
         z = Random.Range(-50, 34);
         Instantiate(enemy, new Vector3(x, 0.5f, z), Quaternion.Euler(-90f, -90f, 0f));
-        yield return null;
+        yield return new WaitForSeconds(spawnTime);
+        StartCoroutine("SpawnEnemy");
     }
 }
