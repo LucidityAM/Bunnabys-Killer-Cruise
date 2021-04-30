0f53ce280fd76df1d99123b1a53c3c15db900fbf
LucidityAM <LucidityAM@gmail.com>
2021-04-29 20:42:24 -0700

    beginning of special weapon

--- Assets/Isaiah/Scripts/Player/Attacks/SpecialWeapon1.cs
+++ Assets/Isaiah/Scripts/Player/Attacks/SpecialWeapon1.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpecialWeapon1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Player Movement")]
+    PlayerController playerController;
+    public float dashSpeed;
+    public float dashTime;
+    public float dashAmount = 3;
+    
+
+    [Header("Telegraphing")]
+    Vector3 position;
+    public Image WeaponTelegraphing;
+
+    [Header("Cooldown")]
+    bool isCooldown;
+    bool canDash = true;
+
+    void Update()
     {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DashAbility()
     {
-        
+        if(Input.GetKey(KeyCode.R) && isCooldown == false)
+        {
+            WeaponTelegraphing.GetComponent<Image>().enabled = true;
+        }
+
+        if (WeaponTelegraphing.GetComponent<Image>().enabled == true && Input.GetKeyUp(KeyCode.R))
+        {
+            if (canDash & dashAmount > 0)
+            {
+                isCooldown = true;
+
+                //StartCoroutine()
+            }
+
+        }
+
     }
 }
