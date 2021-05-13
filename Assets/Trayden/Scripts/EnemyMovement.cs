8da44d6332afd584aa263c8db44ac44e4a646097
SeraphCelestial <trayden@drkmind.com>
2021-05-13 08:51:38 -0700

    Finally added enemy movement

--- Assets/Trayden/Scripts/EnemyMovement.cs
+++ Assets/Trayden/Scripts/EnemyMovement.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public GameObject player;
+    public float speed;
     void Update()
     {
         if(gameObject.GetComponent<CharacterInfo>().health < 100)
         {
             Destroy(gameObject);
         }
+
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
     }
 }
