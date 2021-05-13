c18f9b998efd3691869f91af0f1d934f92951915
TFFavard <tffavard@gmail.com>
2021-05-12 18:12:02 -0700

    pausescript

---
+++ Assets/Vivivivivi/HUD/Scripts/PauseScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    #region things that need to be turned off 
+    public List<MonoBehaviour> multiTypeList;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
