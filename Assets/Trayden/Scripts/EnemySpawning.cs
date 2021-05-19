0f6f4d897d1fb92e7b9643c959ef9107e7a79f9d
SeraphCelestial <trayden@drkmind.com>
2021-05-18 17:54:32 -0700

    enemy prefabs created

--- Assets/Trayden/Scripts/EnemySpawning.cs
+++ Assets/Trayden/Scripts/EnemySpawning.cs
@@ -11,16 +11,26 @@ public class EnemySpawning : MonoBehaviour
     public GameObject waterCat;
     public float x;
     public float z;
+    public int enemyAmount = 1;
+    private int cycleAmount = 0;
     void Start()
     {
         StartCoroutine("SpawnEnemy");
     }
-
-    public IEnumerator SpawnEnemy(int enemyAmount, bool spawnGreenCrew, bool spawnBlueCrew, bool spawnRedCrew, bool spawnCocoCrab, bool spawnWaterCat)
+    public IEnumerator SpawnEnemy()
     {
         x = Random.Range(-20f, 30f);
         z = Random.Range(-47.5f, 29f);
         Instantiate(greenCrew, new Vector3(x, 0.5f, z), Quaternion.Euler(-90f, -90f, 0f));
+        cycleAmount++;
+        if(cycleAmount < enemyAmount)
+        {
+            StartCoroutine("SpawnEnemy");
+        }
+        else
+        {
+
+        }
         yield return null;
     }
 }
