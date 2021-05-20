36f31bd1dbd4cb81d1b6bb5f2722c4682e6d9bd9
SeraphCelestial <trayden@drkmind.com>
2021-05-19 18:06:29 -0700

    Finished wave system

--- Assets/Trayden/Scripts/EnemyMovement.cs
+++ Assets/Trayden/Scripts/EnemyMovement.cs
@@ -7,22 +7,14 @@ public class EnemyMovement : MonoBehaviour
     public GameObject player;
     public float speed;
     public Collider[] hitColliders;
-    public GameObject waveSystem;
     public void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         hitColliders = Physics.OverlapSphere(this.transform.position, 3.5f);
-        waveSystem = GameObject.FindGameObjectWithTag("WaveSystem");
     }
     void Update()
     {
         gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.deltaTime);
-        if(gameObject.GetComponent<CharacterInfo>().health <= 0)
-        {
-            gameObject.tag = "DeadEnemy";
-            waveSystem.GetComponent<EnemySpawning>().enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            Destroy(gameObject);
-        }
 
         for (int i = 0; i < hitColliders.Length; i++)
         {
