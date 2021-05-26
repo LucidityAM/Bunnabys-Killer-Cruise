a206927cfafe3d1c44772ed2f149704cc1ae817c
TFFavard <tffavard@gmail.com>
2021-05-25 18:00:22 -0700

    adding credits cutscene and title

---
+++ Assets/Vivivivivi/CutsceneSkip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneSkip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("EndCutscene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds(33f);
+        SceneManager.LoadScene(1);
+    }
+}
